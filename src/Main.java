788faf7208f5497fdc15f603101cca2929509070
Sergey Lipatov <sergey0737@gmail.com>
2022-06-20 23:47:27 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        int ticket = 2100;  // Цена билета
+        int bonus = 20;
+        System.out.println(" Цена билета " + ticket);
+        int x;
+                x = ticket / bonus;
+        System.out.println(" Бонусных миль " + x);
+
+    }
+
+}
