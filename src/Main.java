a53a0e60af0407f600c97ddb32e5d3cb09d08947
Sergey Lipatov <sergey0737@gmail.com>
2022-06-28 22:24:34 +0300

    BonusMilesService

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Привет");
-        int ticket = 2100;  // Цена билета
-        int bonus = 20;
-        System.out.println(" Цена билета " + ticket);
-        int ratio;
-        ratio = ticket / bonus;
-        System.out.println(" Бонусных миль " + ratio);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
-
 }
